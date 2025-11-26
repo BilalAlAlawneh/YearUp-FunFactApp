28d8856222f1c308cd0329aa677b35f66a80218a
BilalAlAlawneh <bilalalalawneh@gmail.com>
2025-11-26 09:30:12 -0800

    added my fun fact

--- src/main/java/com/pluralsight/FunFactApp/Main.java
+++ src/main/java/com/pluralsight/FunFactApp/Main.java
@@ -5,6 +5,6 @@ package com.pluralsight.FunFactApp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My fun fact:");
-        System.out.println("TODO: replace this text in a Git branch.");
+        System.out.println("C++ was the first language I've learned");
     }
 }
